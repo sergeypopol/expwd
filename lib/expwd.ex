120e7132ee28727cb3aed9f78e0e0c83973bc626
Сергей Попол <popolsergey@gmail.com>
2017-11-27 11:45:09 +0100

    Initial commit

---
+++ lib/expwd.ex
@@ -0,0 +1,18 @@
+defmodule Expwd do
+  @moduledoc """
+  Documentation for Expwd.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Expwd.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/expwd_test.exs
@@ -0,0 +1,8 @@
+defmodule ExpwdTest do
+  use ExUnit.Case
+  doctest Expwd
+
+  test "greets the world" do
+    assert Expwd.hello() == :world
+  end
+end
