8ded22471dd4d4b60ff58a9e9f4fb81645d32a8d
Tangui <29804907+tanguilp@users.noreply.github.com>
2018-10-17 19:14:53 +0300

    Added documentation

--- lib/expwd.ex
+++ lib/expwd.ex
@@ -7,6 +7,38 @@ defmodule Expwd do
 
   def supported_hash_algorithms(), do: @supported_hash_algorithms
 
+  @doc """
+  Securely compare two strings, in constant time.
+
+  Returns `true` if strings are equals, false otherwise
+
+  ## Example
+
+  ```elixir
+  iex> Expwd.secure_compare("V01H2GjdTXE3iqDb+3j1VdbM65+/8QcXcjr9dVuMcYE", "V01H2GjdTXE3iqDb+3j1VdbM65+/8QcXcjr9dVuMcYE")
+  true
+
+  iex> Expwd.secure_compare("V01H2GjdTXE3iqDb+3j1VdbM65+/8QcXcjr9dVuMcYE", "ni8fN4rnwavBlbVpRrD/pYcAulaG4pW33fJ")
+  false
+
+  iex> Expwd.secure_compare("JBCi34pS0x+c9UZEj4itWa9JSqt1UQj9/rfULmpHTRY", %Expwd.Hashed{
+  ...> alg: :sha256,
+  ...> hash: <<205, 221, 101, 226, 145, 163, 216, 198, 62, 105, 203, 181, 200, 103,
+  ...>      13, 217, 120, 77, 212, 27, 113, 80, 122, 148, 104, 73, 29, 200, 97, 58,
+  ...>      143, 60>>
+  ...> })
+  false
+
+  iex> Expwd.secure_compare("JBCi34pS0x+c9UZEj4itWa9JSqt1UQj9/rfULmpHTRY", %Expwd.Hashed{
+  ...> alg: :sha256,
+  ...> hash: <<142, 86, 170, 80, 19, 174, 138, 223, 133, 24, 247, 61, 116, 248, 37,
+  ...>      220, 95, 61, 87, 104, 65, 70, 30, 106, 78, 90, 181, 165, 67, 110, 117,
+  ...>      142>>
+  ...> })
+  true
+
+  ```
+  """
   @spec secure_compare(binary(), binary()) :: boolean()
   def secure_compare(left, right) when is_binary(left) and is_binary(right) do
     hashed_left = :crypto.hash(:sha256, left)
