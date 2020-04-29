774f13fd13d81b179ce955ac47e96b349e8f9c6b
Tangui <29804907+tanguilp@users.noreply.github.com>
2020-04-29 21:39:26 +0300

    Preparing for hex.pm publishing

--- mix.exs
+++ mix.exs
@@ -4,14 +4,17 @@ defmodule Expwd.Mixfile do
   def project do
     [
       app: :expwd,
-      version: "0.1.0",
+      description: "Structure and functions to work with application passwords in Elixir",
+      version: "1.0.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
       docs: [
         main: "readme",
         extras: ["README.md"]
-      ]
+      ],
+      package: package(),
+      source_url: "https://github.com/tanguilp/expwd"
     ]
   end
 
@@ -25,6 +28,13 @@ defmodule Expwd.Mixfile do
     [
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  def package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/tanguilp/expwd"}
     ]
   end
 end
